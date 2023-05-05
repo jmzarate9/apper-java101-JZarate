229d2219e20223d6a150f4e95f6540f55d8c2ca2
Joshua Zarate <joshua.zarate@gcash.com>
2023-05-05 12:12:07 +0800

    Updated Activity1 - CategoryEnum.java

--- day3/CategoryEnum.java
+++ day3/CategoryEnum.java
@@ -5,21 +5,11 @@ public class CategoryEnum {
         double price = 0;
 
         switch (chosenCategory) {
-            case MEN:
-                price = 13.57;
-                break;
-            case WOMEN:
-                price = 20.45;
-                break;
-            case KID:
-                price = 45.6;
-                break;
-            case GRANDMA:
-                price = 56.67;
-                break;
-            case GRANDPA:
-                price = 3.56;
-                break;
+            case MEN: price = 13.57; break;
+            case WOMEN: price = 20.45; break;
+            case KID: price = 45.6; break;
+            case GRANDMA: price = 56.67; break;
+            case GRANDPA: price = 3.56; break;
         }
 
         System.out.println("The price for " +  chosenCategory + " is " + price);
